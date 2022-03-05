f9a73ec72fe4a0389c19685197f1a3a92e38c2ed
EC2 Default User <ec2-user@ip-172-31-14-197.ec2.internal>
2022-03-05 13:53:16 +0000

    7d in production

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import '@fortawesome/fontawesome-free/js/all'
 import "../stylesheets/application" 
 require("jquery")
 
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2022_02_27_080836) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "star"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
